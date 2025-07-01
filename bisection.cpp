f67ebe98c7ab117d7b6776d85f625a7d4c017562
apu-2022831022 <159704411+apu-22@users.noreply.github.com>
2025-07-01 06:59:13 +0000

    update the code structure but same the basic logic.

--- bisection.cpp
+++ bisection.cpp
@@ -3,41 +3,55 @@ using namespace std;
 
 double f(double x)
 {
-    return x * x + 3 * x - 3;
+    return x * x + 3 * x - 3;  
 }
 
 void bisection(double a, double b, double err)
 {
     if (f(a) * f(b) >= 0)
     {
-        cout << "There is no solution between the given interval or there is multiple solution.";
+        cout << "There is no unique root in the given interval or there might be multiple roots." << endl;
         return;
     }
+
     double c;
     int iteration = 0;
 
+    cout << fixed << setprecision(6);
+    cout << "\n-----------------------------------------------------------\n";
+    cout << "Iter\ta\t\tb\t\tc\t\tf(c)\t\t|b-a|\n";
+    cout << "-----------------------------------------------------------\n";
+
     while ((b - a) >= err)
     {
-        c = (b + a) / 2;
+        c = (a + b) / 2.0;
         iteration++;
-        if (c == 0.0)
+
+        cout << iteration << "\t" << a << "\t" << b << "\t" << c << "\t" << f(c) << "\t" << fabs(b - a) << endl;
+
+        // If f(c) is close to 0, we found the root (or close enough)
+        if (fabs(f(c)) <= err)
             break;
-        else if (f(a) * f(c) < 0)
+
+        // Decide the side to repeat the steps
+        if (f(a) * f(c) < 0)
             b = c;
         else
             a = c;
     }
-    cout << "The root of this function is: " << c << endl;
+
+    cout << "\nThe approximate root of the function is: " << c << endl;
+    cout << "Total iterations: " << iteration << endl;
 }
 
 int main()
 {
     double a, b, err;
-    cout << "Enter first interval: " << endl;
+    cout << "Enter first interval (a): ";
     cin >> a;
-    cout << "Enter second interval: " << endl;
+    cout << "Enter second interval (b): ";
     cin >> b;
-    cout << "Enter error : " << endl;
+    cout << "Enter allowed error (e): ";
     cin >> err;
 
     bisection(a, b, err);
