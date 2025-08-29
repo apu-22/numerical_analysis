cb8710f6f339325a7a6a4167df9f139ca3f6b7a3
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-08-29 19:11:21 +0600

    Adde the gauss elimination with partial pivoting.

--- Gauss_Elimination.cpp
+++ Gauss_Elimination.cpp
@@ -45,6 +45,62 @@ void gaussianElimination(vector<vector<double>> &a, vector<double> &b)
     }
 }
 
+void Gauss_Eli_With_Pivoting(vector<vector<double>> &a, vector<double> &b)
+{
+    int n = a.size();
+    cout << n;
+
+    for (int i = 0; i < n; i++)
+    {
+        // partial pivoting
+        int maxr = i;
+        for (int k = i + 1; k < n; k++)
+        {
+            if (fabs(a[k][i]) > fabs(a[maxr][i]))
+                maxr = k;
+        }
+
+        swap(a[i], a[maxr]);
+        swap(b[i], b[maxr]);
+
+        if (fabs(a[i][i]) < 1e-9)
+        {
+            cout << "No unique solution exists (singular matrix).\n";
+            return;
+        }
+
+        // forward elimination
+        for (int k = i + 1; k < n; k++)
+        {
+            double factor = a[k][i] / a[i][i];
+            for (int j = i; j < n; j++)
+            {
+                a[k][j] -= factor * a[i][j];
+            }
+            b[k] -= factor * b[i];
+        }
+    }
+
+    // Backtrac substition
+    vector<double> result(n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        result[i] = b[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            result[i] -= a[i][j] * result[j];
+        }
+        result[i] /= a[i][i];
+    }
+
+    // Output solution
+    cout << "\nSolution:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "x" << i + 1 << " = " << result[i] << endl;
+    }
+}
+
 int main()
 {
     // vamus;
@@ -68,6 +124,12 @@ int main()
         cin >> con[i];
     }
 
+    vector<vector<double>> cef1 = cef;
+    vector<double> con1 = con;
+    // Without partial pivoting
     gaussianElimination(cef, con);
+
+    // With partial pivoting
+    Gauss_Eli_With_Pivoting(cef1, con1);
     return 0;
 }
